98ae1429d922e49be1f1f9f4a9812ab290f107c9
Lap <74074061+ItsLap@users.noreply.github.com>
2024-01-22 17:23:55 +0000

    Update main.js

--- 9. Fine-tuning/main.js
+++ 9. Fine-tuning/main.js
@@ -29,6 +29,13 @@ const traffic=[
     new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2,getRandomColor()),
     new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2,getRandomColor()),
     new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2,getRandomColor()),
+        new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(0),-900,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(2),-900,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(0),-1100,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(1),-1100,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(1),-1300,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(2),-1300,30,50,"DUMMY",2,getRandomColor()),
 ];
 
 animate();
